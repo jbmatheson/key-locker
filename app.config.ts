ede32a6d6d624219a4206fe96d0af69ce28ee932
jimmatheson <jim@rmg.io>
2024-08-26 14:59:32 -0400

    update variables

--- app.config.ts
+++ app.config.ts
@@ -1,28 +1,40 @@
-import { ExpoConfig, ConfigContext } from 'expo/config';
+import { ConfigContext, ExpoConfig } from 'expo/config';
+
+type Env = {
+  EXPO_PUBLIC_SLUG: string;
+  EXPO_PUBLIC_NAME: string;
+  EXPO_PUBLIC_IOS_BUNDLE_IDENTIFIER: string;
+  EXPO_PUBLIC_IOS_ANDROID_PACKAGE: string;
+  EXPO_PUBLIC_PROJECT_ID: string;
+  EXPO_PUBLIC_ENV: string;
+  EXPO_PUBLIC_API_URL: string;
+};
 
 export default ({ config }: ConfigContext): ExpoConfig => {
+  const env: Env = process.env as unknown as Env;
+
   const envConfig: ExpoConfig = {
     ...config,
-    slug: process.env.EXPO_PUBLIC_SLUG ?? '',
-    name: process.env.EXPO_PUBLIC_NAME ?? '',
+    slug: env.EXPO_PUBLIC_SLUG ?? '',
+    name: env.EXPO_PUBLIC_NAME ?? '',
     ios: {
       ...config.ios,
-      bundleIdentifier: process.env.EXPO_PUBLIC_IOS_BUNDLE_IDENTIFIER,
+      bundleIdentifier: env.EXPO_PUBLIC_IOS_BUNDLE_IDENTIFIER,
       buildNumber: '1',
     },
     android: {
       ...config.android,
-      package: process.env.EXPO_PUBLIC_IOS_ANDROID_PACKAGE,
+      package: env.EXPO_PUBLIC_IOS_ANDROID_PACKAGE,
       versionCode: 1,
     },
     updates: {
-      url: `https://u.expo.dev/${process.env.EXPO_PUBLIC_PROJECT_ID}`,
+      url: `https://u.expo.dev/${env.EXPO_PUBLIC_PROJECT_ID}`,
     },
     extra: {
       ...config.extra,
-      eas: { projectId: process.env.EXPO_PUBLIC_PROJECT_ID },
-      ENV: process.env.EXPO_PUBLIC_ENV,
-      API_URL: process.env.EXPO_PUBLIC_API_URL,
+      eas: { projectId: env.EXPO_PUBLIC_PROJECT_ID },
+      ENV: env.EXPO_PUBLIC_ENV,
+      API_URL: env.EXPO_PUBLIC_API_URL,
     },
   };
   return envConfig;
